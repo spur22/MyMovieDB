d101ecd2630119e6297a4779a5e201b8e757ed23
jorsa <jorsa@192.168.1.56>
2018-04-21 17:56:46 +0200

    He cambiado el index

--- BotParaTMDB2/index.php
+++ BotParaTMDB2/index.php
@@ -113,7 +113,7 @@ $n=$a->ordenaraleatorio();
 		<div class="row section-header has-bottom-sep" data-aos="fade-up">
 			<div class="col-full">
 				<h3 class="subhead subhead--dark">MOST SEEN</h3>
-				<h1 class="display-1 display-1--light">hola</h1>
+				<h1 class="display-1 display-1--light">adios</h1>
 			</div>
 		</div>
 		<!-- end section-header -->
